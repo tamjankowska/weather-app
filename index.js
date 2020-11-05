099279bea7797aa850dad7e58dfe232157f79486
Tam Jankowska <tam.jankowska@gmail.com>
2020-11-05 17:11:55 +0000

    Updated API stuff

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const getWeather = require('./lib/getWeather');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('hbs', hbs ({
@@ -19,14 +22,39 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
 
 app.get('/', async (req, res) => {
-    let data = await getWeather();
+    let data = await getWeather('Stockport', 'gb');
     let name = data.name;
+    let description = data.weather[0].description;
     let temp = data.main.temp;
-    res.render('index', {name, temp});
+    let feels_like = data.main.feels_like;
+    res.render('index', {
+        name,
+        data: {description, temp, feels_like}});
 });
 
 app.get('/weather', (req, res) => {
     res.render('weather');
+});
+
+app.post('/weather', async (req, res) => {
+    let city = req.body.city;
+    let code = req.body.code;
+    let data = await getWeather(city, code);
+    if (data.cod == '404') {
+        res.render('weather', {
+            err: 'The provided location doesn\'t exist'
+        });
+        return;
+    }
+    let name = data.name;
+    let description = data.weather[0].description;
+    let temp = data.main.temp;
+    let feels_like = data.main.feels_like;
+    res.render('weather', {
+        name,
+        data: {description, temp, feels_like},
+        listExists: true
+    });
 });
 
 app.get('*', (req, res) => {
